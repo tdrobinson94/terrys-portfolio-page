f81ec61affdc623eadb8516effea9cde28a590dc
tdrobinson94 <tdrobinson94@gmail.com>
2016-09-15 10:05:52 -0400

    style and layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,25 @@
 import $ from 'jquery';
 
 $('.portfolio_link').on('click', function(){
-  $('body').animate({scrollTop: '900px'}, 250);
+  $('body').animate({scrollTop: $('.portfolio').offset().top}, 750);
 });
 
 $('.about_link').on('click', function(){
-  $('body').animate({scrollTop: '300px'}, 250);
+  $('body').animate({scrollTop: $('.about').offset().top}, 750);
+})
+
+$('.contact_link').on('click', function(){
+  $('body').animate({scrollTop: $('.contact').offset().top}, 750);
 })
 
 $('.title').on('click', function(){
-  $('body').animate({scrollTop: '0px'}, 250);
+  $('body').animate({scrollTop: '0px'}, 750);
 })
 
 $('.top_button').on('click', function(){
-  $('body').animate({scrollTop: '0px'}, 250);
+  $('body').animate({scrollTop: '0px'}, 750, "swing");
 })
+
+if ($(window).width() < 650){
+  $('.top_button').hide();
+}
