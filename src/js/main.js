64520fbb737352f6f14137f3c88b93e7503ba536
tdrobinson94 <tdrobinson94@gmail.com>
2016-09-20 08:24:40 -0400

    responsive design tweaks

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,19 @@
 import $ from 'jquery';
 
 $('.portfolio_link').on('click', function(){
-  $('body').animate({scrollTop: $('.portfolio').offset().top}, 750);
+  $('body').animate({scrollTop: $('.portfolio').offset().top}, 1500);
 });
 
 $('.about_link').on('click', function(){
-  $('body').animate({scrollTop: $('.about').offset().top}, 750);
+  $('body').animate({scrollTop: $('.about').offset().top}, 1500);
 })
 
 $('.contact_link').on('click', function(){
-  $('body').animate({scrollTop: $('.contact').offset().top}, 750);
+  $('body').animate({scrollTop: $('.contact').offset().top}, 1500);
 })
 
 $('.title').on('click', function(){
-  $('body').animate({scrollTop: '0px'}, 750);
+  $('body').animate({scrollTop: '0px'}, 1500);
 })
 
 $('.top_button').on('click', function(){
@@ -43,3 +43,22 @@ $(window).resize(function(){
     $('.top_button').show();
   }
 })
+
+$(window).resize(function(){
+  var width = $(window).width();
+  if (width <= 550){
+    $('.link_box').hide();
+  } else {
+    $('.link_box').show();
+  }
+})
+
+$('.menu_tab').on('click', function(){
+  $('.link_box').slideToggle();
+})
+
+if($(window).width() <= 550){
+  $('.link').on('click', function(){
+    $('.link_box').slideToggle();
+  })
+}
