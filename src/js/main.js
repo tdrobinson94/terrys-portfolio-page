1f117de0dad1ef2b9c9dd1a46187e7f87fd8fa8d
tdrobinson94 <tdrobinson94@gmail.com>
2018-07-10 11:07:15 -0400

    fixed top nav scroll links and added new profile image

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,23 @@
 import $ from 'jquery';
 
 $('.portfolio_link').on('click', function(){
-  $('body').animate({scrollTop: $('.portfolio').offset().top}, 750);
+  $('body, html').animate({scrollTop: $('.portfolio').offset().top}, 750);
 });
 
 $('.about_link').on('click', function(){
-  $('body').animate({scrollTop: $('.about').offset().top}, 750);
+  $('body, html').animate({scrollTop: $('.about').offset().top}, 750);
 })
 
 $('.contact_link').on('click', function(){
-  $('body').animate({scrollTop: $('.contact').offset().top}, 750);
+  $('body, html').animate({scrollTop: $('.contact').offset().top}, 750);
 })
 
 $('.title').on('click', function(){
-  $('body').animate({scrollTop: '0px'}, 750);
+  $('body, html').animate({scrollTop: '0px'}, 750);
 })
 
 $('.top_button').on('click', function(){
-  $('body').animate({scrollTop: '0px'}, 750);
+  $('body, html').animate({scrollTop: '0px'}, 750);
 })
 
 // $('.one').on('click', function(){
